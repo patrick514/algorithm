b4ee77cb843300d45567b7546f95072c2de1e1e1
trick Pa <2528726609@qq.com>
2025-03-17 14:32:12 +0800

    binarysearch

--- src/分类/二分查找/33.ts
+++ src/分类/二分查找/33.ts
@@ -1,34 +1,63 @@
+// function search(nums: number[], target: number): number {
+
+//     //  [4,5,6,7,0,1,2]  总有一边是有序的
+//     let left = 0
+//     let right = nums.length - 1
+//     while (left <= right) {
+//         let mid = Math.floor((right - left) / 2) + left
+//         if (nums[mid] === target) {
+//             return mid
+//         }
+
+//         //左半部分有序 注意取等
+//         if (nums[left] <= nums[mid]) {
+//注意  后面取等
+//             //在 左半里
+//             if (target < nums[mid] && target >= nums[left]) {
+//                 right = mid - 1
+//             //不在 左半
+//             } else {
+//                 left = mid + 1
+//             }
+//         }
+//         //右半部分有序
+//         else {
+//             if (target > nums[mid] && target <= nums[right]) {
+//                 left = mid + 1
+//             } else {
+//                 right = mid - 1
+//             }
+
+//         }
+//     }
+//     return -1
+// };
+
 function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left <= right) {
+    let mid = Math.floor((right - left) / 2 + left);
 
-    //  [4,5,6,7,0,1,2]
-    let left = 0
-    let right = nums.length - 1
-    while (left <= right) {
-        let mid = Math.floor((right - left) / 2) + left
-        if (nums[mid] === target) {
-            return mid
-        }
-
-        //左半部分有序
-        if (nums[left] <= nums[mid]) {
-            //在 左半里
-            if (target < nums[mid] && target >= nums[left]) {
-                right = mid - 1
-            //不在 左半
-            } else {
-                left = mid + 1
-            }
-        }
-        //右半部分有序
-        else {
-            if (target > nums[mid] && target <= nums[right]) {
-                left = mid + 1
-            } else {
-                right = mid - 1
-            }
-
-        }
+    if (nums[mid] === target) {
+      return mid;
     }
-    return -1
-};
+    if (nums[left] <= nums[mid]) {
+      //注意  后面取等
+      if (target < nums[mid] && target >= nums[left]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+console.log(search([5, 1, 3], 3));
--- src/分类/二分查找/74.ts
+++ src/分类/二分查找/74.ts
@@ -1,27 +1,57 @@
-function searchMatrix(matrix: number[][], target: number): boolean {
-    if (!matrix.length || !matrix[0].length) {
-        return false
-    }
-    let m = matrix.length
-    let n = matrix[0].length
+// function searchMatrix(matrix: number[][], target: number): boolean {
+//     if (!matrix.length || !matrix[0].length) {
+//         return false
+//     }
+//     let m = matrix.length
+//     let n = matrix[0].length
 
-    let left = 0
-    let right = m * n - 1
+//     let left = 0
+//     let right = m * n - 1
 
-    while (left <= right) {
-        let mid = Math.floor((right - left) / 2) + left
-        let midvalue = matrix[Math.floor(mid / n)][mid %n]
-        if(midvalue === target){
-            return true
-        }
-        else if(midvalue < target){
-            left = mid + 1
+//     while (left <= right) {
+//         let mid = Math.floor((right - left) / 2) + left
+//                                         关键 行    列
+//         let midvalue = matrix[Math.floor(mid / n)][mid %n]
+//         if(midvalue === target){
+//             return true
+//         }
+//         else if(midvalue < target){
+//             left = mid + 1
+//         }else{
+//             right = mid -1
+//         }
+//     }
+//     return false
+
+// };
+function flat(nums:any[]):any[]{
+    let result:any[] = []
+    nums.forEach(item => {
+        if(Array.isArray(item)){
+            result = result.concat(flat(item))
         }else{
-            right = mid -1
+            result.push(item)
+        }
+    });
+    return result
+}
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const mat = flat(matrix)
+    let left = 0
+    let right = mat.length-1
+    while(left <= right){
+        let mid = Math.floor((right-left)/2 + left)
+        if(mat[mid] === target){
+            return true
+        }else if(mat[mid] < target){
+            left = mid  +1
+        }else{
+            right  = mid -1
         }
     }
     return false
-
 };
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]],3))
 
-searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]],3)
+
+
