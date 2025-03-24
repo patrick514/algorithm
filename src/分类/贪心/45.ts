f897c659f801168339bd28a505b5fc65357262e1
patrick514 <2528726609@qq.com>
2025-03-24 11:11:37 +0800

    greedy

--- src/分类/贪心/45.ts
+++ src/分类/贪心/45.ts
@@ -1,33 +1,51 @@
+// function jump(nums: number[]): number {
+//     let jumpcnt = 0
+//     // let max = 0
+//     // let end = nums.length
+//     // for (let i = end - 2; i >= 0; i--) {
+//     //     if (i === 0) {
+//     //         end = end - max
+//     //         i = end-1
+//     //         jumpcnt++
+//     //     }
+//     //     if (nums[i] + i >= end - 1) {
+
+//     //         if (nums[i] >= max) {
+//     //             max = nums[i]
+//     //         }
+
+//     //     }
+
+//     // }
+//     let farest = 0
+//     let edge = 0
+//     for(let i = 0;i<nums.length-1;i++){
+//         farest = Math.max(farest,i+nums[i])
+//         if(i === edge){
+//             jumpcnt++
+//             // i = farest
+//             edge = farest
+//         }
+//     }
+//     return jumpcnt
+// };
+
+
 function jump(nums: number[]): number {
-    let jumpcnt = 0
-    // let max = 0
-    // let end = nums.length
-    // for (let i = end - 2; i >= 0; i--) {
-    //     if (i === 0) {
-    //         end = end - max
-    //         i = end-1
-    //         jumpcnt++
-    //     }
-    //     if (nums[i] + i >= end - 1) {
-
-    //         if (nums[i] >= max) {
-    //             max = nums[i]
-    //         }
-
-    //     }
-
-    // }
-    let farest = 0
-    let edge = 0
-    for(let i = 0;i<nums.length-1;i++){
-        farest = Math.max(farest,i+nums[i])
-        if(i === edge){
-            jumpcnt++
-            // i = farest
-            edge = farest
-        }
+  let farest = 0; //下一座 更长的桥 的长度
+  let cnt = 0; //建桥的次数
+  let bridge = 0; // 已建的桥
+  // 当 i=n−2 时，如果 i<curRight，说明可以到达 n−1；如果 i=curRight，我们会造桥，这样也可以到达 n−1。
+  for (let i = 0; i < nums.length - 1; i++) {
+    farest = Math.max(farest, nums[i] + i);
+    //无路可走 必须建桥
+    if (i === bridge) {
+      cnt++;
+      bridge = farest; // 建桥后的新长度
     }
-    return jumpcnt
+  }
+
+  return cnt;
 };
 console.log(jump([2, 3, 1, 1, 4]))
 console.log(jump([2, 3, 0, 1, 4]))
--- src/分类/贪心/55.ts
+++ src/分类/贪心/55.ts
@@ -1,17 +1,44 @@
+// function canJump(nums: number[]): boolean {
+//     // let pass = false
+
+//     for(let i = nums.length - 1;i >= 0;i--){
+//         if(nums[i] + i >= nums.length-1 ){
+//             nums.length = i+1
+//         }
+//     }
+//     if(nums.length === 1){
+//         return true
+//     }
+
+//     return false
+// };
+
+//如果可以从起点跳到终点，那么一定存在一条路径
+// function canJump(nums: number[]): boolean {
+//   let lastPos = nums.length - 1;
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     if (nums[i] + i >= lastPos) {
+//       lastPos = i;
+//     }
+//   }
+//   return lastPos === 0;
+// }
+
 function canJump(nums: number[]): boolean {
-    // let pass = false
-
-
-    for(let i = nums.length - 1;i >= 0;i--){
-        if(nums[i] + i >= nums.length-1 ){
-            nums.length = i+1
-        }
+  let maxLen = 0;
+  for (let i = 0; i < nums.length; i++) {
+    // 如果当前能到的最大距离  小于 下一个 则不行
+    if (i <= maxLen) {
+      maxLen = Math.max(maxLen, nums[i] + i);
+      if (maxLen >= nums.length - 1) {
+        return true;
+      }
     }
-    if(nums.length === 1){
-        return true
+    else{
+      return false
     }
-
-    return false
-};
-console.log(canJump([2, 3, 1, 1, 4]))
-console.log(canJump([3, 2, 1, 0, 4]))
+  }
+  return false;
+}
+console.log(canJump([0, 3, 1, 1, 4]));
+console.log(canJump([3, 2, 1, 0, 4]));
